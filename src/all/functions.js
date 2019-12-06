fa88c34f35d413880936a9a65885fc6d0be45d04
SMCodesprivate <samuelpvp150@gmail.com>
2019-12-06 02:21:18 +0000

    1.0.1

---
+++ src/all/functions.js
@@ -0,0 +1,31 @@
+import api from '../services/api.js';
+async function verify_login() {
+    var x = true;
+    var user = JSON.parse(localStorage.getItem('user_infos'));
+    if(user === null) {
+        x = false;
+    } else {
+        var infos = await api.post("/is", { username: user.username });
+        if(infos.data.error === true) {
+            localStorage.clear();
+            x = false;
+        } else {
+            var test = await api.post("/veriencripty", { username: user.username, password: user.password });
+            if(test.data.result === false) {
+                localStorage.clear();
+                x = false;
+            }
+        }
+    }
+    return x;
+}
+async function verify_staff() {
+    var user = JSON.parse(localStorage.getItem("user_infos"));
+    var staff = await api.post("/is", { username: user.username });
+    var x = false;
+    if(staff.data.returno.level > 0) {
+        x = true;
+    }
+    return x;
+}
+export { verify_staff, verify_login };
